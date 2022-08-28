61fe061bc246e22c115a1e565debf45e5b7fce18
Linalii <36931084+Linalii@users.noreply.github.com>
2022-08-28 14:39:11 +0300

    Add files via upload

---
+++ Task 001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Задай первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задай второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == numberB*numberB){
+    Console.WriteLine(numberA+" квадрат "+numberB);
+}
+else Console.WriteLine(numberA+" не квадрат "+numberB);
