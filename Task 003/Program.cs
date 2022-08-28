1598692a9c8f76d20a10d8454807a40e494ce767
Linalii <36931084+Linalii@users.noreply.github.com>
2022-08-28 14:40:51 +0300

    Add files via upload

---
+++ Task 003/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Задай первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задай второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задай третье число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Max(numberС,Math.Max(numberA, numberB)));
+
---
+++ Task 004/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Задай первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if(numberA%2==0){
+Console.WriteLine("Четное");
+}
+else Console.WriteLine("Нечетное");
